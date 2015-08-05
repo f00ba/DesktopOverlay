cc49a7ed8571d06a6f13533ce469e7d692040d2f
Nico Poier <nico.poier1995@yahoo.de>
2015-08-05 13:43:31 +0200

    Tkinter version

--- __main__.py
+++ __main__.py
@@ -1,8 +1,31 @@
-import wx
+from tkinter import *
 from elements.hexagon import Hex
 
-app = wx.App()
+root = Tk()
 
-one_hex = Hex(None, -1, 'test', (0, 0))
-one_hex.Show()
-app.MainLoop()
+root.image = PhotoImage(file='hex.png')
+"""
+clear_screen = Label(root, image=None, width=1920, height=1080)
+fr = Frame(root, width=102, height=90, bg="#00ff00")
+label1 = Label(fr, image=root.image, width=102, height=90, bg='white')
+label1.pack()
+newWindow = Toplevel(root)
+fr.place(x=0, y=0, width=102, height=90)
+fr2 = Frame(newWindow, width=102, height=90, bg="#0000ff")
+label2 = Label(fr2, image=root.image, width=102, height=90, bg='white')
+label2.pack()
+fr2.place(x=80, y=0, width=102, height=90)
+"""
+
+hex1 = Hex(root, 0, 0, root.image)
+hex2 = Hex(root, 102, 90, root.image, "faggot")
+
+root.configure(background='white')
+root.geometry("1920x1080+0+0")
+#root.overrideredirect(True)
+root.wm_attributes("-topmost", True)
+#root.wm_attributes("-disabled", True)
+root.wm_attributes("-transparentcolor", "white")
+
+
+root.mainloop()
--- elements/hexagon.py
+++ elements/hexagon.py
@@ -1,14 +1,24 @@
-import wx
+from tkinter import *
 
-class Hex(wx.Frame):
+class Hex:
 
-    def __init__(self, parent, id, title, pos):
-        wx.Frame.__init__(self, parent, id, title, pos, size=(102, 90))
+    def __init__(self, parent, x, y, image, title="Test"):
 
-        self.panel = wx.Panel(self, -1)
-        self.bitmap = wx.Bitmap("hex.png")
-        self.image = wx.StaticBitmap(self.panel, -1, self.bitmap)
+        self.window = Toplevel(parent)
+        self.window.config(width=102, height = 90)
 
-        self.image.SetPosition((0,0))
+        self.window.title(title)
 
-        self.SetTransparent(255)
+        #self.window.overrideredirect(True)
+        self.window.configure(background='white')
+        self.window.wm_attributes("-topmost", True)
+        #self.window.wm_attributes("-disabled", True)
+        self.window.wm_attributes("-transparentcolor", "white")
+
+        self.frame = Frame(self.window, width=102, height=90, bg="#00ff00")
+
+        self.hexImg = Label(self.frame, image=image, width=102, height=90, bg='white')
+        self.hexImg.pack()
+
+        self.frame.place(x=x, y=y)
+
